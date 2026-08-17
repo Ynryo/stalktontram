d55d413c194866beae8c4dbe4f305c5438da9a44
Ynryo <ewenatideklein@gmail.com>
2026-08-17 13:04:16 +0200

    feat: Introduce `SquareLinearLayout` and update platform label layout
    Key changes:
    - Implemented `SquareLinearLayout`, a custom view that ensures its width is at least equal to its measured height during the layout pass.
    - Updated `vehicle_stop_details.xml` to use `SquareLinearLayout` for the platform indicator container (`llPlatformContainer`) to ensure consistent dimensions.

---
+++ app/src/main/java/fr/ynryo/stalktonbus/views/SquareLinearLayout.java
@@ -0,0 +1,37 @@
+package fr.ynryo.stalktonbus.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class SquareLinearLayout extends LinearLayout {
+
+    public SquareLinearLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * Appelé par le layout manager pour déterminer la taille de la vue avant l'affichage
+     * Modifie la largeur pour qu'elle soit égale à la hauteur
+     *
+     * @param widthMeasureSpec  Mesure de la largeur
+     * @param heightMeasureSpec Mesure de la hauteur
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int height = getMeasuredHeight();
+        int width = Math.max(getMeasuredWidth(), height);
+        setMeasuredDimension(width, height);
+    }
+}
